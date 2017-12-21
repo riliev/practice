75c871be00ec0a5464673a2213f46088ce5ef66c
Rumen Iliev <rumen.i.iliev@gmail.com>
2017-12-21 06:14:10 -0800

    adding KNN regression

--- statistical_learning_R/trials.R
+++ statistical_learning_R/trials.R
@@ -18,10 +18,27 @@ lda.pred=predict(lda.fit, Smarket.2005)
 names(lda.pred)
 lda.class=lda.pred$class
 table(lda.class,Direction.2005)
-mean(lda.class==Direction.2005)
-sum(lda.pred$posterior[,1]>=.5)
+mean(lda.class==Direction.2005) # this measures the accuracy of the  prediction
+sum(lda.pred$posterior[,1]>=.5) # .5 is the threshold for computing the accuracy
 sum(lda.pred$posterior[,1]<.5)
 lda.pred$posterior[1:20,1]
 lda.class[1:20]
 sum(lda.pred$posterior[,1]>.9)
+# Quadratic linear analysis 
+# I'll skip that for now
+
+# K-Nearest Neighbors
+
+library(class)
+train.X=cbind(Smarket$Lag1,Smarket$Lag2)[train,]
+test.X=cbind(Smarket$Lag1,Smarket$Lag2)[!train,]
+train.Direction=Smarket$Direction[train]
+set.seed(1)
+knn.pred=knn(train.X,test.X,train.Direction,k=1)
+table(knn.pred,Direction.2005)
+(83+43)/252
+knn.pred=knn(train.X,test.X,train.Direction,k=3)
+table(knn.pred,Direction.2005)
+mean(knn.pred==Direction.2005)
+
 #
